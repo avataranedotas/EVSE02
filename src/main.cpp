958accd6062bd9dda0bdbd6891486697df6fc28a
Alexandre Moleiro <avataranedotas@yahoo.com>
2019-10-26 15:12:00 +0100

    Auto-reset

--- src/main.cpp
+++ src/main.cpp
@@ -14,12 +14,38 @@ const char *password = "zzzzzzzz";
 
 int amperes = 6;
 int amperes_ant = 0;
-int contasegundos = 0;
+unsigned long contasegundos = 0;
+unsigned long ucontasegundos = 0;
+int contafalhas = 0;
+bool falhou = false;
 
 char buffer[100];
 
 unsigned long timer1, timer2, timer3, timer4, timer5, timer6, timer7, timer8 = 0;
-unsigned long delayStart = 0;
+unsigned long primeirafalha = 0;
+
+volatile int interruptCounter;
+int totalInterruptCounter;
+ 
+hw_timer_t * timer = NULL;
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
+ 
+//watchdog
+void IRAM_ATTR onTimer()
+{
+  portENTER_CRITICAL_ISR(&timerMux);
+  Serial.println("Interrupt");
+  if (contasegundos != ucontasegundos)
+    ucontasegundos = contasegundos;
+  else
+  {
+    esp_task_wdt_init(1, true);
+    esp_task_wdt_add(NULL);
+    while (true)
+      ;
+  }
+  portEXIT_CRITICAL_ISR(&timerMux);
+}
 
 void hard_restart() {
   esp_task_wdt_init(1,true);
@@ -32,6 +58,12 @@ void hard_restart() {
 
 void setup()
 {
+
+
+  timer = timerBegin(0, 80, true);
+  timerAttachInterrupt(timer, &onTimer, true);
+  timerAlarmWrite(timer, 5000000, true);  //interrupt a cada 5 segundos
+  timerAlarmEnable(timer);
 
   int aux_temp;
   // put your setup code here, to run once:
@@ -56,7 +88,7 @@ void setup()
 
   //esperar um pouco e piscar rapido
 
-  while (millis() < 1000)
+  while (millis() < 500)
   {
 
     aux_temp = millis() % 100;
@@ -101,8 +133,8 @@ void loop()
 
   if (millis() - timer1 > 1000)
   {
-    Serial.print(millis());
-    Serial.print("  Amperes a enviar:");
+    //Serial.print(millis());
+    Serial.print("Amperes a enviar:");
     Serial.println(amperes);
 
     //liga led
@@ -115,10 +147,10 @@ void loop()
 
     contasegundos = contasegundos + 1;
 
-    if (contasegundos > 300) {
-      hard_restart();
-
-    }
+    //if (contasegundos > 300) {
+    //  hard_restart();
+    //
+    //}
 
     //se Wifi estiver ligado
     if ((WiFi.status() == WL_CONNECTED) /*& (amperes!=amperes_ant)*/)
@@ -139,13 +171,19 @@ void loop()
         Serial.println(amperes);
         Serial.print("Resultado: ");
         Serial.println(httpCode); //200 = OK
-        Serial.println(amperes);
+        //Serial.println(amperes);
         //Serial.println(payload);
+
+        if (httpCode==200) contafalhas = 0;
+        else contafalhas = contafalhas + 1;
+
+
       }
 
       else
       {
         Serial.println("Error on HTTP request");
+        contafalhas = contafalhas + 1;
       }
 
       http.end(); //Free the resources
@@ -171,8 +209,13 @@ void loop()
 
       //WiFi.disconnect(true,false);
 
-      while ((millis() % 2001)!=0) {};
-      while ((millis() % 1999)!=0) {};
+      //while ((millis() % 2001)!=0) {};
+      //while ((millis() % 1999)!=0) {};
+
+      contafalhas = contafalhas + 1;
+      Serial.print("Contagem falhas:");
+      Serial.println(contafalhas);
+
 
       WiFi.reconnect();
 
@@ -187,5 +230,67 @@ void loop()
 
       timer1 = millis();
   }
+
+  //ciclo 0.75s
+
+  if (millis() - timer2 > 750)
+  {
+    Serial.print("Millis:");
+    Serial.println(millis());
+
+    if ((contafalhas!=0)) 
+    {
+      Serial.print("Falhas:");
+      Serial.println(contafalhas);
+    } 
+
+
+    if ((contafalhas!=0) & (!falhou)) 
+    {
+      falhou = true;
+      primeirafalha = millis();
+      Serial.print("Primeira falha:");
+      Serial.println(primeirafalha);
+      
+    }
+
+    if ((falhou) & (millis()>(primeirafalha + 10000)) )
+    {
+      Serial.println("Reset");
+      hard_restart();
+    }
+
+
+    if (contafalhas == 0)
+    {
+      falhou = false;
+    
+    }
+
+
+      //if (contafalhas > 10 )  //se já tiverem passado 10 segundos faz restart
+      //{
+      //  hard_restart();
+      //}
+
+
+
+      timer2 = millis();
+  }
+
+  /*
+  if (interruptCounter > 0) {
+ 
+    portENTER_CRITICAL(&timerMux);
+    interruptCounter--;
+    portEXIT_CRITICAL(&timerMux);
+ 
+    totalInterruptCounter++;
+ 
+    Serial.print("An interrupt as occurred");
+    Serial.println(totalInterruptCounter);
+ 
+  }
+  */
 
 }
